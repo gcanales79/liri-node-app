886f1f651b45db8cc63ecd33b3d62991ae8cfc18
Gustavo Canales <gustavo.canales@me.com>
2018-06-19 18:30:42 -0500

    With Bonus

--- liri.js
+++ liri.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 var Spotify = require('node-spotify-api')
-
+var Twitter = require('twitter');
 var keys = require("./keys.js");
+var fs = require("fs");
 
 
 //var spotify = (keys.spotify);
@@ -14,13 +15,69 @@ var liriFunction = process.argv[2];
 
 //Select what function to run 
 if (liriFunction == "movie-this") {
+    fs.appendFile("log.txt", "\nCommand run: " + liriFunction, function (err) {
+        if (err) {
+            return console.log(err)
+        }
+    });
     movieLiri();
 }
+else {
+    if (liriFunction == "spotify-this-song") {
+        fs.appendFile("log.txt", "\nCommand run: " + liriFunction, function (err) {
+            if (err) {
+                return console.log(err)
+            }
+        });
+        spotifyLiri();
+    }
+    else {
+        if (liriFunction == "my-tweets") {
+            fs.appendFile("log.txt", "\nCommand run: " + liriFunction, function (err) {
+                if (err) {
+                    return console.log(err)
+                }
+            });
+            tweetLiri();
+        }
+        else {
+            if (liriFunction == "do-what-it-says") {
+                fs.appendFile("log.txt", "\nCommand run: " + liriFunction, function (err) {
+                    if (err) {
+                        return console.log(err)
+                    }
+                });
+                fs.readFile("random.txt", "utf8", function (err, data) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    var commandArr = data.split(",")
+                    //console.log(commandArr);
+                    if (commandArr[0] == "movie-this") {
+                        infoMovie(commandArr[1]);
 
-if (liriFunction == "spotify-this-song") {
-    spotifyLiri();
+                    }
+                    if (commandArr[0] == "spotify-this-song") {
+                        infoSong(commandArr[1]);
+                    }
+                    if (commandArr[0] == "my-tweets") {
+                        tweetLiri();
+
+                    }
+
+                })
+            }
+            else {
+                console.log("I didn't understand your command");
+                fs.appendFile("log.txt", "\nCommand run: " + liriFunction, function (err) {
+                    if (err) {
+                        return console.log(err)
+                    }
+                });
+            }
+        }
+    }
 }
-
 
 function movieLiri() {
     // Create an empty variable for holding the movie name
@@ -85,16 +142,32 @@ function movieLiri() {
                     }
 
                     // Parse the body of the site and recover the info
+                    var Title= "The Title of the Movie is: " + JSON.parse(body).Title;
+                    var Year="The release year was: " + JSON.parse(body).Year;
+                    var Rating= "The IMDB rating is: " + JSON.parse(body).imdbRating;
+                    var tomatoesRating= "The Rotten Tomatoes Rating is: " + rottenTomatesrating;
+                    var Country= "The country is was produce is: " + JSON.parse(body).Country;
+                    var Language="The language of the movie is: " + JSON.parse(body).Language;
+                    var Plot= "The plot of the movie is: " + JSON.parse(body).Plot;
+                    var Actors="The actors are: " + JSON.parse(body).Actors;
 
-                    console.log("The Title of the Movie is: " + JSON.parse(body).Title);
-                    console.log("The release year was: " + JSON.parse(body).Year);
-                    console.log("The IMDB rating is: " + JSON.parse(body).imdbRating);
-                    console.log("The Rotten Tomatoes Rating is: " + rottenTomatesrating);
-                    console.log("The country is was produce is: " + JSON.parse(body).Country);
-                    console.log("The language of the movie is: " + JSON.parse(body).Language);
-                    console.log("The plot of the movie is: " + JSON.parse(body).Plot);
-                    console.log("The actors are: " + JSON.parse(body).Actors);
+                    console.log(Title);
+                    console.log(Year);
+                    console.log(Rating);
+                    console.log(tomatoesRating);
+                    console.log(Country);
+                    console.log(Language);
+                    console.log(Plot);
+                    console.log(Actors);
 
+                    addLog(Title);
+                    addLog(Year);
+                    addLog(Rating);
+                    addLog(tomatoesRating);
+                    addLog(Country);
+                    addLog(Language);
+                    addLog(Plot);
+                    addLog(Actors);
                 }
             }
         });
@@ -119,10 +192,21 @@ function infoSong(songName) {
             console.log("Some suggestions are: ")
             for (var i = 0; i < numberOfSearch; i++) {
 
-                console.log("\nThe song name is: " + data.tracks.items[i].name)
-                console.log("The album of the song is: " + data.tracks.items[i].album.name)
-                console.log("The artist is: " + data.tracks.items[i].artists[0].name)
-                console.log("A preview link of the song can be found in: " + data.tracks.items[i].external_urls.spotify)
+                var name= "The song name is: " + data.tracks.items[i].name;
+                var album="The album of the song is: " + data.tracks.items[i].album.name;
+                var artist="The artist is: " + data.tracks.items[i].artists[0].name;
+                var link="A preview link of the song can be found in: " + data.tracks.items[i].external_urls.spotify;
+
+                addLog(name);
+                addLog(album);
+                addLog(artist);
+                addLog(link);
+
+                console.log("\n" + name);
+                console.log(album);
+                console.log(artist);
+                console.log(link);
+               
             }
         }
         else {
@@ -166,4 +250,56 @@ function spotifyLiri() {
 
         infoSong(songName)
     }
+}
+
+
+//Function to get the tweets
+
+function tweetLiri() {
+    var client = new Twitter(keys.twitter);
+
+    client.get('statuses/user_timeline', function (error, tweets, response) {
+        if (error) throw error;
+        //console.log(JSON.stringify(tweets,null,2));  // The tweets
+        //console.log(tweets.length);
+        //console.log(JSON.stringify(response))   ;  // Raw response object.
+
+        function showTweets() {
+            for (var i = 0; i < tweets.length; i++) {
+                var numberOftweet = i + 1;
+                var tweetText= "Tweet: " + tweets[i].text;
+                var tweetDate="Created in: " + tweets[i].created_at;
+
+                addLog(tweetText);
+                addLog(tweetDate);
+
+
+                console.log("\n Tweet " + numberOftweet);
+                console.log("\n" + tweetText);
+                console.log(tweetDate);
+             
+            }
+        }
+        if (tweets.length < 20) {
+            var responseNumber = tweets.length;
+            console.log("Here are your " + responseNumber + " tweets");
+            showTweets();
+        }
+
+        if (tweets.lenght >= 20) {
+            console.log("Your last 20 tweets are:");
+            showTweets();
+        }
+
+
+    });
+
+}
+
+function addLog(logText){
+    fs.appendFile("log.txt","\n " + logText, function (err) {
+        if (err) {
+            return console.log(err)
+        }
+    })
 }
